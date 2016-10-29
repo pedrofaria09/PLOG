475c6b085329cb5d105003c9e472296bc5c5ecae
Pedro Faria <pfaf09@gmail.com>
2016-10-29 16:45:59 +0100

    Tested

--- PLOG - ORDO/main.pl
+++ PLOG - ORDO/main.pl
@@ -4,22 +4,22 @@
 :-use_module(library(lists)).
 
 %Jogada Impar - Joga as Brancas - ' O '
-jogada(NumeroJogada,L1) :- impar(NumeroJogada),
-	gameArea(NumeroJogada, L1),
+jogada(NumeroJogada,AtualBoard) :- impar(NumeroJogada),
+	gameArea(NumeroJogada, AtualBoard),
 	askTypeOfMove(TypeOfMove),
-	(TypeOfMove == 1 -> simpleWhiteMove(NumeroJogada, L1, NewBoard2); true),
-	(TypeOfMove == 2 -> askWhiteOrdoNrMoves(NumeroJogada, L1, NewBoard2, NewBoard3); true),
+	(TypeOfMove == 1 -> simpleWhiteMove(NumeroJogada, AtualBoard, NewBoard); true),
+	(TypeOfMove == 2 -> askWhiteOrdoNrMoves(NumeroJogada, AtualBoard, NewBoard, NewBoard3); true),
 	Y is NumeroJogada+1,
 	jogada(Y, NewBoard3).
 
 %Jogada Par - Joga as Pretas - ' X '
-jogada(NumeroJogada,L1) :- par(NumeroJogada),
-	gameArea(NumeroJogada, L1),
+jogada(NumeroJogada,AtualBoard) :- par(NumeroJogada),
+	gameArea(NumeroJogada, AtualBoard),
 	askTypeOfMove(TypeOfMove),
-	(TypeOfMove == 1 -> simpleBlackMove(NumeroJogada, L1, NewBoard2); true),
-	(TypeOfMove == 2 -> askBlackOrdoNrMoves(NumeroJogada, L1, NewBoard2); true),
+	(TypeOfMove == 1 -> simpleBlackMove(NumeroJogada, AtualBoard, NewBoard); true),
+	(TypeOfMove == 2 -> askBlackOrdoNrMoves(NumeroJogada, AtualBoard, NewBoard); true),
 	Y is NumeroJogada+1,
-	jogada(Y,NewBoard2).
+	jogada(Y,NewBoard).
 
 endOfGame(NumeroJogada):- impar(NumeroJogada),cls, write('Jogador Branco ganha.'),!.
 endOfGame(NumeroJogada):- par(NumeroJogada),cls, write('Jogador Preto ganha.'),!.
@@ -28,44 +28,43 @@ askTypeOfMove(TypeOfMove) :- write('Escolha tipo de jogada: '), nl,
 	write('1 - Simples'), nl,
 	write('2 - Ordo'), nl,
 	getDigit(TypeOfMove).
-	
-askBlackOrdoNrMoves(NumeroJogada, L1, NewBoard2):-	write('Numero de pecas a mover: '), nl,
+
+askBlackOrdoNrMoves(NumeroJogada, AtualBoard, NewBoard):-	write('Numero de pecas a mover: '), nl,
 	getDigit(NrMoves),
-	ordoBlackMove(NrMoves, NumeroJogada, L1, NewBoard2).
-	
-askWhiteOrdoNrMoves(NumeroJogada, L1, NewBoard2, NewBoard3):-	write('Numero de pecas a mover: '), nl,
+	ordoBlackMove(NrMoves, NumeroJogada, AtualBoard, NewBoard).
+
+askWhiteOrdoNrMoves(NumeroJogada, AtualBoard, NewBoard, NewBoard3):-	write('Numero de pecas a mover: '), nl,
 	getDigit(NrMoves),
-	ordoWhiteMove(NrMoves, NumeroJogada, L1, NewBoard2, NewBoard3).
-	
-simpleWhiteMove(NumeroJogada, L1, NewBoard2) :- askPlay(OldX, OldY, NewX, NewY, NumeroJogada, L1),
+	ordoWhiteMove(NrMoves, NumeroJogada, AtualBoard, NewBoard, NewBoard3).
+
+simpleWhiteMove(NumeroJogada, AtualBoard, NewBoard) :- askPlay(OldX, OldY, NewX, NewY, NumeroJogada, AtualBoard),
 	letterToNumber(OldX, OldXNumber),
 	letterToNumber(NewX, NewXNumber),
-	getElement(L1, OldY, OldXNumber, OldElement),
-	getElement(L1, NewY, NewXNumber, NewElement),
-	verifySimpleWhiteMove(NewElement,OldElement, NumeroJogada, L1, NewElement2, OldY, NewY),
-	changeBoard(NewElement2, OldXNumber, OldY, L1, NewBoard1),
-	changeBoard(OldElement, NewXNumber, NewY, NewBoard1, NewBoard2),
-	connected(NewBoard2, L1, NewXNumber, NewY, OldElement, NumeroJogada),
+	getElement(AtualBoard, OldY, OldXNumber, OldElement),
+	getElement(AtualBoard, NewY, NewXNumber, NewElement),
+	verifySimpleWhiteMove(NewElement,OldElement, NumeroJogada, AtualBoard, NewElement2, OldY, NewY),
+	changeBoard(NewElement2, OldXNumber, OldY, AtualBoard, NewBoard1),
+	changeBoard(OldElement, NewXNumber, NewY, NewBoard1, NewBoard),
+	connected(NewBoard, AtualBoard, NewXNumber, NewY, OldElement, NumeroJogada),
 	(NewY == 1 -> cls, endOfGame(NumeroJogada); true).
-	
-simpleBlackMove(NumeroJogada, L1, NewBoard2) :- askPlay(OldX, OldY, NewX, NewY, NumeroJogada, L1),
+
+simpleBlackMove(NumeroJogada, AtualBoard, NewBoard) :- askPlay(OldX, OldY, NewX, NewY, NumeroJogada, AtualBoard),
 	letterToNumber(OldX, OldXNumber),
 	letterToNumber(NewX, NewXNumber),
-	getElement(L1, OldY, OldXNumber, OldElement),
-	getElement(L1, NewY, NewXNumber, NewElement),
-	verifySimpleBlackMove(NewElement,OldElement, NumeroJogada, L1, NewElement2, OldY, NewY),
-	changeBoard(NewElement2, OldXNumber, OldY, L1, NewBoard1),
-	changeBoard(OldElement, NewXNumber, NewY, NewBoard1, NewBoard2),
-	connected(NewBoard2, L1, NewXNumber, NewY, OldElement, NumeroJogada),
+	getElement(AtualBoard, OldY, OldXNumber, OldElement),
+	getElement(AtualBoard, NewY, NewXNumber, NewElement),
+	verifySimpleBlackMove(NewElement,OldElement, NumeroJogada, AtualBoard, NewElement2, OldY, NewY),
+	changeBoard(NewElement2, OldXNumber, OldY, AtualBoard, NewBoard1),
+	changeBoard(OldElement, NewXNumber, NewY, NewBoard1, NewBoard),
+	connected(NewBoard, AtualBoard, NewXNumber, NewY, OldElement, NumeroJogada),
 	(NewY == 8 -> cls, endOfGame(NumeroJogada); true).
-	
-ordoBlackMove(NrMoves, NumeroJogada, L1, NewBoard2) :- ((NrMoves == 0) -> true;
-	simpleBlackMove(NumeroJogada, L1, NewBoard2),
-	Y is NrMoves-1,
-	ordoBlackMove(Y, NumeroJogada, NewBoard2, NewBoard2)).
 
-ordoWhiteMove(NrMoves, NumeroJogada, L1, NewBoard2, NewBoard3) :- ((NrMoves == 0) -> true;
-	simpleWhiteMove(NumeroJogada, L1, NewBoard2),
+ordoBlackMove(NrMoves, NumeroJogada, AtualBoard, NewBoard) :- ((NrMoves == 0) -> true;
+	simpleBlackMove(NumeroJogada, AtualBoard, NewBoard),
 	Y is NrMoves-1,
-	ordoWhiteMove(Y, NumeroJogada, NewBoard2, NewBoard3, NewBoard3)).
+	ordoBlackMove(Y, NumeroJogada, NewBoard, NewBoard)).
 
+ordoWhiteMove(NrMoves, NumeroJogada, AtualBoard, NewBoard, NewBoard3) :- ((NrMoves == 0) -> true;
+	simpleWhiteMove(NumeroJogada, AtualBoard, NewBoard),
+	Y is NrMoves-1,
+	ordoWhiteMove(Y, NumeroJogada, NewBoard, NewBoard3, NewBoard3)).
